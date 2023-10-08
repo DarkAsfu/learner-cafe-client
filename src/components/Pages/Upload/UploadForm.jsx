6839dea6092625c6bc2f0b681837bdb4d987c417
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-10-08 13:12:19 +0600

    add loading:

--- src/components/Pages/Upload/UploadForm.jsx
+++ src/components/Pages/Upload/UploadForm.jsx
@@ -62,7 +62,6 @@ const UploadForm = () => {
                     })
                         .then(res => res.json())
                         .then(data => {
-                            console.log(data);
                             if (data.insertedId) {
                                 Swal.fire(
                                     'Thank you!',
